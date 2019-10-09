9fdfd52107d06733214be60b4b2da3aa88ca9517
Lea Goarant <leagoarant@msn.com>
2019-10-09 11:50:28 +0200

    initializing

---
+++ src/main/java/com/wcs/darkcircus/DarkCircusApplication.java
@@ -0,0 +1,13 @@
+package com.wcs.darkcircus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkCircusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkCircusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wcs/darkcircus/DarkCircusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wcs.darkcircus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DarkCircusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
